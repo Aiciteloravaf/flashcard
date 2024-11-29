0d14d337552ded6524f354791ac029d86c60bc9a
Aiciteloravaf <leticiaeliasgrerfavaro@gmail.com>
2024-11-30 02:05:58 +0800

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Hello Kitty',
+    'Com qual fruta a Hello Kitty é associada?',
+    'Com uma maçã, a qual está sempre junto dos produtos comerciais, para sempre remetermos a personagem com a fruta'
+)
+
+criaCartao(
+    'Qúmica',
+    'O que é um polímero?',
+    'Os polímeros de adição são macromoléculas formadas a partir de conjuntos de monômeros (moléculas menores)'
+)
+
+criaCartao(
+    'Hello Kitty',
+    'Quais são os nomes dos personagens do desenho animado Hello Kitty and friends?',
+    'Hello Kitty, My Melody, Kuromi, Keroppi, Badz-maru, Pompompurin, Cinnamoroll e Chococat'
+)
+
+criaCartao(
+    'Química',
+    'O que se encontra em um átomo?',
+    'Prótons, elétrons e nêutrons'
+)
